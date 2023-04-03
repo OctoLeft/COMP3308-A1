622faddf23a08520fdcb5db8908c910855a555ef
Craig Wang <zwan3215@uni.sydney.edu.au>
2023-04-03 14:08:02 +1000

    Task 4

---
+++ Task4/task4.py
@@ -0,0 +1,8 @@
+def task4(algorithm, message_filename, dictionary_filename, threshold, letters, debug):
+    pass
+
+if __name__ == '__main__':
+    # Example function calls below, you can add your own to test the task4 function
+    print(task4('d', 'cabs.txt', 'common_words.txt', 100, 'ABC', 'y'))
+    print(task4('b', 'cabs.txt', 'common_words.txt', 100, 'ABC', 'y'))
+    print(task4('i', 'cabs.txt', 'common_words.txt', 100, 'ABC', 'y'))
